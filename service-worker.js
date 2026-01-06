ea69de2dafe47caf2cbdec6c3b2f17dfee5eaa63
AKASHA HUB <yanfili.simon@gmail.com>
2026-01-06 12:29:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,20 @@
-self.addEventListener("install", function (event) {
-  console.log("App Akasha instalado");
+const CACHE_NAME = "akasha-cache-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
 });
 
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
